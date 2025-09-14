f21df769795c030c93e9275f688ba0f0e239c863
AwareFoxy <135021509+AwareFoxy@users.noreply.github.com>
2025-09-15 01:37:55 +0300

    Update publish_multi_request.py

--- Tools/publish_multi_request.py
+++ Tools/publish_multi_request.py
@@ -15,7 +15,7 @@ RELEASE_DIR = "release"
 # CONFIGURATION PARAMETERS
 # Forks should change these to publish to their own infrastructure.
 #
-ROBUST_CDN_URL = "http://188.92.78.98:27960/"
+ROBUST_CDN_URL = "http://cdn.axolotl-station.ru/"
 FORK_ID = "axolotl"
 
 def main():
